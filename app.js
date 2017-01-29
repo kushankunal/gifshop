f458531f248a5696e097734f0dbf147bedf39219
Kunal Prasad <kprasad@hq-mac-kprasad-3.local>
2017-01-28 19:35:04 -0800

    add rest query

--- app.js
+++ app.js
@@ -8,6 +8,44 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var router = express.Router();
+var pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres';
+
+var app = express();
+
+router.post('/api/v1/add', (req, res, next) => {
+  console.warn("in post add")
+  const results = [];
+  // Grab data from http request
+  var data = {gifname: req.body.gifname, username: req.body.username, targerurl: req.body.targeturl};
+  console.warn(data)
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    console.warn("got connection")
+    // SQL Query > Insert Data
+    client.query('INSERT INTO posts(gifname, username, targeturl) values($1, $2, $3)',
+    [data.gifname, data.username, data.targeturl]);
+    // SQL Query > Select Data
+    var query = client.query('SELECT * FROM posts ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
 app.get('/test', function(req, res) {
 	console.log('query', req.query)
 	console.log('body', req.body)
